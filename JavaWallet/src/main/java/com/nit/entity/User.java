1af5b61e56bf565398b74745d9d031bf033456a4
E-Keerti <keertiejjari5@gmail.com>
2025-03-21 11:17:48 +0530

    Entity class created

---
+++ JavaWallet/src/main/java/com/nit/entity/User.java
@@ -0,0 +1,61 @@
+package com.nit.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private BigDecimal balance;
+
+    public User() {
+    }
+
+    public User(String name, BigDecimal balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public void addMoney(BigDecimal amount) {
+        this.balance = this.balance.add(amount);
+    }
+
+    public boolean withdrawMoney(BigDecimal amount) {
+        if (this.balance.compareTo(amount) >= 0) {
+            this.balance = this.balance.subtract(amount);
+            return true;
+        }
+        return false;
+    }
+}
