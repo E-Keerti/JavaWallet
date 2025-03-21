a693a5dd66a9ec818577b5557d666ddfc1257ba4
E-Keerti <keertiejjari5@gmail.com>
2025-03-21 11:13:03 +0530

    Project created

---
+++ JavaWallet/src/main/java/com/nit/JavaWalletApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWalletApplication.class, args);
+	}
+
+}
---
+++ JavaWallet/src/test/java/com/nit/JavaWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
